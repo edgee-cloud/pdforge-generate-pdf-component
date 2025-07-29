d836f0e94834f0fcfd3daca9fd8cbd4086ea22c1
Alex Casalboni <alex@edgee.cloud>
2025-07-29 09:45:18 +0200

    update helpers

--- src/pdforge_payload.rs
+++ src/pdforge_payload.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-const PDFORGE_ENDPOINT: &str = "https://api.pdforge.com/v1/pdf/sync";
-
 #[derive(Serialize, Deserialize)]
 pub struct PdforgeGeneratePdfPayload {
     #[serde(rename = "templateId")]
@@ -11,20 +9,24 @@ pub struct PdforgeGeneratePdfPayload {
 }
 
 impl PdforgeGeneratePdfPayload {
-    pub fn new(pdf_data: serde_json::Value, template_id: &str) -> Self {
+    pub fn new(pdf_data: &serde_json::Value, template_id: &str) -> Self {
         Self {
-            pdf_data,
+            pdf_data: pdf_data.clone(),
             template_id: template_id.to_string(),
         }
     }
 
+    #[cfg(not(test))]
+    const PDFORGE_ENDPOINT: &str = "https://api.pdforge.com/v1/pdf/sync";
+
+    #[cfg(not(test))]
     pub fn send(&self, api_key: &str) -> anyhow::Result<waki::Response> {
         // the body is url-encoded
 
         // call the PDForge API
         let client = waki::Client::new();
         let response = client
-            .post(PDFORGE_ENDPOINT)
+            .post(Self::PDFORGE_ENDPOINT)
             .header("Content-Type", "application/json")
             .header("Authorization", format!("Bearer {api_key}"))
             .body(serde_json::to_string(&self)?)
